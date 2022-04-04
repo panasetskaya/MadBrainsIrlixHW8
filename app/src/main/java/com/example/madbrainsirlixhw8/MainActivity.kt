839f0f2a6da976dfcae18cfe3a6216c4350f9008
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-04-04 11:37:12 +0800

    added: button listener with firebase messaging

--- app/src/main/java/com/example/madbrainsirlixhw8/MainActivity.kt
+++ app/src/main/java/com/example/madbrainsirlixhw8/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.madbrainsirlixhw8
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.madbrainsirlixhw8.databinding.ActivityMainBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
@@ -21,10 +22,21 @@ class MainActivity : AppCompatActivity() {
             checkGooglePlayServices()
             FirebaseMessaging.getInstance().token.addOnCompleteListener( OnCompleteListener{
                 if (!it.isSuccessful) {
-                    Log.i("MyResult","")
+                    Log.i("MyResult","fetching token failed", it.exception)
+                    return@OnCompleteListener
                 }
+                val token = it.result
+                Log.d("TAG1" ,token)
+                Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
             })
-
         }
+    }
+
+    private fun checkGooglePlayServices() {
+        TODO("Not yet implemented")
+    }
+
+    private fun createNotificationChannel() {
+        TODO("Not yet implemented")
     }
 }
