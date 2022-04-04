e90b4599a62cc372d5696b67c7e09a666755bec7
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-04-04 11:50:29 +0800

    added: notication channel, google service checking

--- app/src/main/java/com/example/madbrainsirlixhw8/MainActivity.kt
+++ app/src/main/java/com/example/madbrainsirlixhw8/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.madbrainsirlixhw8
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.madbrainsirlixhw8.databinding.ActivityMainBinding
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+
+    val CHANNEL_ID = "1"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -32,11 +40,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkGooglePlayServices() {
-        TODO("Not yet implemented")
+    private fun checkGooglePlayServices(): Boolean {
+        val status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        return if (status!=ConnectionResult.SUCCESS) {
+            Log.i("TAG1", "Error")
+            false
+        } else {
+            Log.i("TAG1", "Google Play Services updated")
+            true
+        }
     }
 
     private fun createNotificationChannel() {
-        TODO("Not yet implemented")
+        val name = "My Gorgeous Channel"
+        val descriptionText = "My gorgeous description"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
     }
 }
